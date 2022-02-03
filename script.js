17546199cf66c209fb4bd0ff4f130acd56df643d
mafe <mariferpg19@gmail.com>
2022-02-03 16:52:54 -0500

    paso 3

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hola")
+function search_gif(){
+
+    let valor= document.getElementById("searchbar").value
+    let previos= document.getElementById("sugerencias")
+    previos.innerHTML="";
+    console.log(valor)
+   fetch ("https:/api.giphy.com/v1/gifs/search/tags?api_key=U11CfHnhf9zlOH9TXLGYgbXjmOx3mjw9&q="+valor).then(results)
+    function results(resultado){
+        resultado.json().then(informacion)
+      }
+    function informacion(data){
+        console.log(data.data[0]);
+            for (let i= 0; i< data.data.length; i++) {
+                const gif =data.data[i];
+                const gifDiv=document.createElement("div");
+                gifDiv.innerHTML=gif.name;
+                previos.appendChild(gifDiv);
+            }
+    }
+    console.log("hola soy mafe")
+}
+
